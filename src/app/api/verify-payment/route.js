fe3ec49a77e11dc795d7a48c3eb288d7765afe7c
yashraj5656 <yashrajsingh3876@gmail.com>
2025-10-02 13:55:33 +0530

    commit

--- src/app/api/verify-payment/route.js
+++ src/app/api/verify-payment/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import crypto from "crypto";
-import { connectDB } from "@/lib/mongodb";
-import User from "@/models/User";
+
 
 export async function POST(req) {
   try {
